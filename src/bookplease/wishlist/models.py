e00e466e00b728c1b72e48a79486abf04ebe72fb
Anki <anki@Ankis-MacBook-Pro.local>
2018-08-19 12:22:30 -0700

    schemas defined

--- src/bookplease/wishlist/models.py
+++ src/bookplease/wishlist/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=80)
+    password = models.CharField(max_length=80)
+    token = models.CharField(max_length=40)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    isbn = models.CharField(max_length=17)
+    publication_date = models.DateTimeField()
+
+class BookWish(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    date_wished = models.DateTimeField(true)
+    date_granted = models.DateTimeField()
