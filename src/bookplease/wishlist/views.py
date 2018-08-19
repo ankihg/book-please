3e81a4cec21bc5f71c8cfe342bbad060d21ddd7e
Anki <anki@Ankis-MacBook-Pro.local>
2018-08-19 13:15:47 -0700

    hello books

--- src/bookplease/wishlist/views.py
+++ src/bookplease/wishlist/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import User, Book, BookWish
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_book_list = Book.objects.order_by('-date_published')[:5]
+    output = ', '.join([b.title for b in latest_book_list])
+    return HttpResponse(output)
