40ed9d30884f6b626f5a4c02d0a1b06659dbf2b7
Anki <anki@Ankis-MacBook-Pro.local>
2018-08-21 10:11:21 -0700

    cleanup

--- src/bookplease/wishlist/views.py
+++ src/bookplease/wishlist/views.py
@@ -23,36 +23,18 @@ def add_book_to_wish_list(request):
     # output = ', '.join([b.title for b in latest_book_list])
     # return HttpResponse(output)
 
-    # mydata = json.loads(request.body.decode("utf-8"))
-    # print(typeof(mydata))
-    # print(mydata['user_id'])
-
-    print(request.body)
     body_unicode = request.body.decode('utf-8')
-    print('body_unicode')
-    print(body_unicode)
-    # print(body_unicode)
     body = json.loads(body_unicode)
     print('print body')
     print(body)
-    print(body['user_id'])
+
 
     book_wish = BookWish(user_id=body['user_id'], book_id=body['book_id'], date_wished=timezone.now())
     print(book_wish)
     book_wish.save()
-    # book_wish_json = serializers.serialize('json', [ book_wish ])
-    book_wish_json = serializers.serialize('json', BookWish.objects.all())
+    book_wish_json = serializers.serialize('json', [ book_wish ])
+    # book_wish_json = serializers.serialize('json', BookWish.objects.all())
 
-    # book_wish_json = json.loads(book_wish.encode('utf-8'))
     print(book_wish_json)
 
-    # print(body)
-    # body = json.loads('{"plz": "respond"}')
-    # body = {}
-    # body['plz'] = 'respond'
-    # return HttpResponse(json.dumps(body))
     return HttpResponse(book_wish_json)
-
-    content = body['content']
-    print(content)
-    return HttpResponse(content)
