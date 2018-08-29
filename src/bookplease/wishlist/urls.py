4b59eaba60583699f37d3084e0c82fcd248b44dd
Anki <anki@Ankis-MacBook-Pro.local>
2018-08-28 18:56:42 -0700

    organie urls

--- src/bookplease/wishlist/urls.py
+++ src/bookplease/wishlist/urls.py
@@ -4,13 +4,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    path('bookWishes', views.add_book_to_wish_list, name='wish for book'),
-    path('bookWishes/books/<book_id>', views.cancel_book_wish, name='cancel book wish'),
+    # User routes
     path('users/register', views.register_user, name='register user'),
     path('users/login', views.login_user, name='login user'),
-    path('users/<user_id>/bookWishes', views.get_user_book_wishes, name='get user book wishes'),
+
+    # Book routes
     path('books', views.get_books, name='get books'),
+
+    # BookWish routes
+    path('users/<user_id>/bookWishes', views.get_user_book_wishes, name='get user book wishes'),
+    path('bookWishes', views.add_book_to_wish_list, name='wish for book'),
+    path('bookWishes/books/<book_id>', views.cancel_book_wish, name='cancel book wish'),
     path('bookWishes/books/<book_id>/grant', views.mark_book_wish_as_granted, name='mark book wish as granted'),
-    # url(r'^books/(?P<author>\w{0,50})', views.get_books_by_author, name='get books by author'),
 ]
